6f779806605d0fd9844ed060069db032a17f6d8e
Luan Lazzari <luanlazzari@hotmail.com>
2023-02-17 14:32:48 -0300

    chore: project begin

---
+++ src/test/java/com/inconsistency/javakafka/kafkajava/KafkaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inconsistency.javakafka.kafkajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
