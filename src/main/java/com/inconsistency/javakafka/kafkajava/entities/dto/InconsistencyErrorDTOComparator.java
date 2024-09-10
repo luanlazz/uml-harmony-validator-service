3b2642009e2093aeb206944875fd73723b51add0
luanlazz <luanlazzari@hotmail.com>
2024-09-10 01:22:23 -0300

    chore: order inconsistencies by concentration and element

--- src/main/java/com/inconsistency/javakafka/kafkajava/entities/dto/InconsistencyErrorDTOComparator.java
+++ src/main/java/com/inconsistency/javakafka/kafkajava/entities/dto/InconsistencyErrorDTOComparator.java
@@ -2,18 +2,13 @@ package com.inconsistency.javakafka.kafkajava.entities.dto;
 
 import java.util.Comparator;
 
+import org.apache.commons.lang.builder.CompareToBuilder;
+
 public class InconsistencyErrorDTOComparator implements Comparator<InconsistencyErrorDTO> {
 
 	@Override
 	public int compare(InconsistencyErrorDTO o1, InconsistencyErrorDTO o2) {
-		if (o1 == null && o2 == null) {
-			return 0;
-		} else if (o1 == null) {
-			return -1;
-		} else if (o2 == null) {
-			return 1;
-		} else {
-			return o1.getSeverity() - o2.getSeverity();
-		}
+		return new CompareToBuilder().append(o1.getConcentration(), o2.getConcentration())
+				.append(o1.getElId(), o2.getElId()).toComparison();
 	}
 }
