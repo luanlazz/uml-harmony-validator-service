faf09840e905011caed45d426efe60e8f275566f
luanlazz <luanlazzari@hotmail.com>
2026-07-24 01:50:37 -0300

    feat: route to sign the stream

--- src/main/java/com/inconsistency/javakafka/kafkajava/controller/ModelValidatorController.java
+++ src/main/java/com/inconsistency/javakafka/kafkajava/controller/ModelValidatorController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
@@ -12,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +24,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.inconsistency.javakafka.kafkajava.analyse.service.AnalyseModel;
+import com.inconsistency.javakafka.kafkajava.analyse.service.SseNotificationService;
 import com.inconsistency.javakafka.kafkajava.controller.dto.InconsistenciesResponse;
 import com.inconsistency.javakafka.kafkajava.entities.uml.dto.UMLModelDTO;
 import com.inconsistency.javakafka.kafkajava.uml.reader.service.UMLModelReaderService;
@@ -36,12 +40,16 @@ import lombok.RequiredArgsConstructor;
 public class ModelValidatorController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ModelValidatorController.class);
+	private static final Long SSE_TIMEOUT = 300000L;
 
 	private final AtomicLong counter = new AtomicLong();
 
 	@Autowired
 	private AnalyseModel analyseUMLModelService;
 
+	@Autowired
+    private SseNotificationService sseNotificationService;
+	
 	@ResponseBody
 	@PostMapping()
 	public ResponseEntity<Map<String, String>> validate(@RequestParam("file") MultipartFile file, Locale locale) {
@@ -70,6 +78,27 @@ public class ModelValidatorController {
 
 		return new ResponseEntity<Map<String, String>>(responseBody, HttpStatus.BAD_REQUEST);
 	}
+	
+	@GetMapping(value = "/stream/{clientId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter streamResults(@PathVariable String clientId) {
+        SseEmitter emitter = new SseEmitter(SSE_TIMEOUT);
+        sseNotificationService.addEmitter(clientId, emitter);
+        
+        Optional<InconsistenciesResponse> cached = sseNotificationService.getCachedResult(clientId);
+        if (cached.isPresent()) {
+            try {
+                emitter.send(SseEmitter.event().name("analysis-complete").data(cached.get()));
+                emitter.complete();
+            } catch (IOException e) {
+                logger.error("Error sending cached result", e);
+            } finally {
+            	sseNotificationService.removeEmitter(clientId);
+            }
+        }
+        
+        return emitter;
+    }
+	
 	private File convertToFile(MultipartFile file) throws IOException {
 		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 		File convFile = new File(System.getProperty("java.io.tmpdir") + "/" + fileName);
